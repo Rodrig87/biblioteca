972ad96d715df7c4181003c1a5a923a90a74c8a3
Rodrigo Azevedo <134436718+Rodrig87@users.noreply.github.com>
2023-10-10 08:50:58 -0300

    Update biblioteca.py

--- biblioteca.py
+++ biblioteca.py
@@ -10,3 +10,27 @@ def diminuir(num1, num2):
 def multi(num1, num2):
     resp = num1 * num2
     print(resp)
+def piramide(qtd):
+    for x in range(qtd+1):
+        print(str(x) * x)
+def piramide_espaço(qtd):
+    for x in range(1,qtd+1):
+        for y in range(1,x+1):
+            print(y, end=" ")
+        print()
+def total_vogais(texto):
+    contador = 0
+    for x in texto:
+        if x in "aeiouAEIOU":
+            contador+=1
+    print(f"O total de vogais é de {contador}")
+def estoque(produto,qtd,valor):
+    estoqueTotal = qtd * valor
+    return estoqueTotal
+def pos_neg(num):
+    if num > 0:
+        return ("P")
+    elif num < 0:
+        return ("N")
+    else:
+        return ("Z")

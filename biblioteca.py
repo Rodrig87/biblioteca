c7eb69ab5c50ad2d717c1774a32f85539b6a0b21
Rodrigo Azevedo <134436718+Rodrig87@users.noreply.github.com>
2023-10-10 08:50:15 -0300

    Add files via upload

---
+++ biblioteca.py
@@ -0,0 +1,12 @@
+def somar(num1, num2):
+    resp = num1 + num2
+    print(resp)
+def dividir(num1, num2):
+    resp = num1 / num2
+    print(resp)
+def diminuir(num1, num2):
+    resp = num1 - num2
+    print(resp)
+def multi(num1, num2):
+    resp = num1 * num2
+    print(resp)
